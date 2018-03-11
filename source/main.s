fef95bfbcb49001f4af76bdcb45ada1fc9836bc5
Jeremydude101 <32525400+Jeremydude101@users.noreply.github.com>
2018-03-11 12:29:21 -0600

    .

--- source/main.s
+++ source/main.s
@@ -1,11 +1,7 @@
 //This program was created by Zheyu Jeremy Ying (30002931) and Zachary Metz (30001506)
 
-@ Data section
-.section    .data
-
-
 Names: //string format to print names
-  .asciz  "This Program was created by Zheyu Jeremy Ying (30002931) and Zachary Metz (30001506)"
+  .asciz  "This Program was created by Zheyu Jeremy Ying (30002931) and Zachary Metz (30001506)\n"
 
 InputPrompt:
   .asciz "Please press a button...\n"
@@ -47,9 +43,6 @@ ButtonRight:
 
 ButtonLeft:
   .asciz "LEFT bumper"
-
-@ Code section
-.section    .text
 
 .align 4
 .global main
