abaf0340cefce01c40bb66293debc7763cfa4b9d
Zachary Metz <zachary.metz@ucalgary.ca>
2018-03-10 19:01:13 -0700

    base files

---
+++ source/main.s
@@ -0,0 +1,17 @@
+//This program was created by Zheyu Jeremy Ying (30002931) and Zachary Metz (30001506)
+
+
+@ Data section
+.section    .data
+
+
+Names: //string format to print names
+  .asciz  "This Program was created by Zheyu Jeremy Ying (30002931) and Zachary Metz (30001506)"
+
+
+@ Code section
+.section    .text
+
+.align 4
+.global main
+main:
