1ef4e59693a7b8f3c103bf7147113d722ca2271a
Jeremydude101 <32525400+Jeremydude101@users.noreply.github.com>
2018-03-11 14:59:47 -0600

    added Button Check

--- source/main.s
+++ source/main.s
@@ -15,7 +15,7 @@ JoyPadRight:
 JoyPadLeft:
   .asciz "Joy-pad LEFT\n"
 
-JoyPadUpt:
+JoyPadUp:
 .asciz "Joy-pad UP\n"
 
 JoyPadDown:
@@ -45,6 +45,9 @@ ButtonRight:
 ButtonLeft:
   .asciz "LEFT bumper\n"
 
+TerminateString:
+  .asciz "Program is terminating\n"
+
 .align 4
 .global main
 main:
@@ -72,133 +75,179 @@ main:
   mov r1, #1
   bl Init_GPIO
 
-  mov r0, #12
-  bl delayMicroseconds
+  PrintInputPrompt:
+    ldr r0, =InputPrompt
+    bl printf
 
-  b haltLoop
+  ButtonCheck:
+    bl Read_SNES
+    cmp r0, #0xFFFF
+    beq ButtonCheck
 
-Init_GPIO:
-  mov r3, r0 //move pin number to r3
+    mov r10, #0
+    ButtonPress:
+      ands wzr, r0, #0x1
+      beq //last digit 0
+      mov r0, r10
+      bl printPressed
+      b ButtonPressLoopCheck
 
-  mov r10, #10
-  sdiv r0, r10
-  mov r10, #4
-  mul r0, r10
+      bne //last digit 1
+      add r10, r10, #1 //loop counter
+      b ButtonPressLoopCheck
 
-  ldr	r7, =gpioBaseAddress
-  ldr	gBase, [r7]
-  add gBase, gBase, r0
+      ButtonPressLoopCheck:
+        cmp r10, 16
+        blt ButtonPress
 
-  mov r2, #7 //b0111
+        b PrintInputPrompt
 
-  mov r10, #3
-  mul r3, r10 //multiply by 3 for first bit
-  lsl r2, r3
-  bic gBase, r2
+    b haltLoop
 
-  mov pc, lr //ret
+  //array index in r0
+  printPressed:
+    push {lr}
+    mov r0, r1
+    ldr r0, =buttonPress_m
+    ldr r0, [r0, r1] //load at offset
+    bl printf
 
-Read_Data:
-  //mov r0, #10 //data pin
-  mov r0, #1
+    cmp r1, #3 //check if it's start
+    beq TerminateProgram
+    b NotStart
 
-  ldr r1, =gpioBaseAddress
-  ldr r1, [r1, #52] //GPLEV0
-  mov r3, #1
-  lsl r3, r0 //align to Data (pin 10)
-  and r1, r3 //mask everything
-  teq r1, #0
-  moveq r0, #0 //return 0
-  movne r0, #1 //return 1
+    TerminateProgram:
+      ldr r0, =TerminateString
+      bl printf
+      b haltLoop
 
-  mov pc, lr //ret
+    NotStart:
+
+    pop {lr}
+    mov pc, lr
 
 
-// writes a bit to the snes latch
-// r1, value to write either a 1 or a 0
-Write_Latch:
-  //load base address
-  ldr r2, =gpioBaseAddress
-  mov r3, #1
-  lsl r3, #9 // align bit for pin#9
-  teq r1, #0
-  streq r3, [r2, #40] // set set pin to 0
-  Strne r3, [r2, #28] // set pin to 1
+  Init_GPIO:
+    mov r3, r0 //move pin number to r3
 
-  mov pc, lr // return
+    mov r10, #10
+    sdiv r0, r10
+    mov r10, #4
+    mul r0, r10
 
-// writes a bit to the snes clock
-// r1, value to write either a 1 or a 0
-Write_Clock:
-  //load base address of pin 11
-  ldr r2, =gpioBaseAddress
-  add r2, 4 // add the offset
-  mov r3, #1
-  lsl r3, #2 // align bit for pin#11 (11-9=2)
-  teq r1, #0
-  streq r3, [r2, #40] // set set pin to 0
-  Strne r3, [r2, #28] // set pin to 1
+    ldr	r7, =gpioBaseAddress
+    ldr	gBase, [r7]
+    add gBase, gBase, r0
 
-  mov pc, lr // return
+    mov r2, #7 //b0111
 
-Read_SNES:
-  mov r10, #0 //register sampling Button
+    mov r10, #3
+    mul r3, r10 //multiply by 3 for first bit
+    lsl r2, r3
+    bic gBase, r2
 
-  mov r1, #1 //input
-  bl Write_Clock
+    mov pc, lr //ret
 
-  mov r1, #1 //input
-  bl Write_Latch
+  Read_Data:
+    //mov r0, #10 //data pin
+    mov r0, #1
 
-  mov r0, #12
-  bl delayMicroseconds
+    ldr r1, =gpioBaseAddress
+    ldr r1, [r1, #52] //GPLEV0
+    mov r3, #1
+    lsl r3, r0 //align to Data (pin 10)
+    and r1, r3 //mask everything
+    teq r1, #0
+    moveq r0, #0 //return 0
+    movne r0, #1 //return 1
 
-  mov r1, #0 //input
-  bl Write_Latch
+    mov pc, lr //ret
 
-  mov r9, #0 //loop counter
 
-  ClockLoop:
-    mov r0, #6
-    bl delayMicroseconds
+  // writes a bit to the snes latch
+  // r1, value to write either a 1 or a 0
+  Write_Latch:
+    //load base address
+    ldr r2, =gpioBaseAddress
+    mov r3, #1
+    lsl r3, #9 // align bit for pin#9
+    teq r1, #0
+    streq r3, [r2, #40] // set set pin to 0
+    Strne r3, [r2, #28] // set pin to 1
 
-    mob r1, #1 //input
+    mov pc, lr // return
+
+  // writes a bit to the snes clock
+  // r1, value to write either a 1 or a 0
+  Write_Clock:
+    //load base address of pin 11
+    ldr r2, =gpioBaseAddress
+    add r2, 4 // add the offset
+    mov r3, #1
+    lsl r3, #2 // align bit for pin#11 (11-9=2)
+    teq r1, #0
+    streq r3, [r2, #40] // set set pin to 0
+    Strne r3, [r2, #28] // set pin to 1
+
+    mov pc, lr // return
+
+  Read_SNES:
+    push {lr}
+    mov r10, #0 //register sampling Button
+
+    mov r1, #1 //input
     bl Write_Clock
 
-    mov r0, #6
+    mov r1, #1 //input
+    bl Write_Latch
+
+    mov r0, #12
     bl delayMicroseconds
 
-    bl Read_Data
+    mov r1, #0 //input
+    bl Write_Latch
 
-    add r10, r10, r0 //add 1 or 0 to buttons
-    lsl r10, #1 //shift 1 over
+    mov r9, #0 //loop counter
 
-    mov r1, #1
-    bl Write_Clock
+    ClockLoop:
+      mov r0, #6
+      bl delayMicroseconds
 
-    add r9, r9, #1 //incrememnt loop counter
-    cmp r9, #16
-    blt ClockLoop
+      mob r1, #1 //input
+      bl Write_Clock
 
-  mov r0, r10 //return pressed buttons
-  mov pc, lr
+      mov r0, #6
+      bl delayMicroseconds
+
+      bl Read_Data
+
+      add r10, r10, r0 //add 1 or 0 to buttons
+      lsl r10, #1 //shift 1 over
+
+      mov r1, #1
+      bl Write_Clock
+
+      add r9, r9, #1 //incrememnt loop counter
+      cmp r9, #16
+      blt ClockLoop
+
+    mov r0, r10 //return pressed buttons
+
+    pop {lr}
 
 
 
 
-
-
-
-
-
-  mov pc, lr //ret
+    mov pc, lr //ret
 
 
 haltLoop:
   b haltLoop
 
-.section .Data
+.Data
 
   .global gpioBaseAddress
   gpioBaseAddress:
   	.int	0
+
+  buttonPress_m: .word ButtonB, ButtonY, ButtonSelect, ButtonStart, JoyPadUp, JoyPadDown, JoyPadLeft, JoyPadRight, ButtonA, ButtonX, ButtonLeft, ButtonRight
