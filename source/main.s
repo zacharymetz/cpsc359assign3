b3a0907ddab7326005c47acdf8bfbf5870c7a9af
Zachary Metz <zachary.metz@ucalgary.ca>
2018-03-10 19:10:36 -0700

    added print strings

--- source/main.s
+++ source/main.s
@@ -8,6 +8,54 @@
 Names: //string format to print names
   .asciz  "This Program was created by Zheyu Jeremy Ying (30002931) and Zachary Metz (30001506)"
 
+InputPrompt:
+  .asciz "Please press a button...\n"
+
+InputResultPre:
+  .asciz "You have pressed "
+
+JoyPadRight:
+  .asciz "Joy-pad RIGHT"
+
+JoyPadLeft:
+  .asciz "Joy-pad LEFT"
+
+JoyPadUpt:
+.asciz "Joy-pad UP"
+
+JoyPadDown:
+  .asciz "Joy-pad DOWN"
+
+ButtonX:
+  .asciz "X"
+ButtonY:
+  .asciz "Y"
+
+ButtonA:
+  .asciz "A"
+
+ButtonB:
+  .asciz "B"
+
+ButtonStart:
+.asciz "Start"
+
+ButtonSelect:
+  .asciz "Select"
+
+ButtonRight:
+.asciz "RIGHT bumper"
+
+ButtonLeft:
+  .asciz "LEFT bumper"
+
+
+
+
+
+
+
+
 
 @ Code section
 .section    .text
